954f5e601811c75a1b675f6e8750d28e718abbd1
ffxz <244880113@qq.com>
2020-04-10 22:38:51 +0800

    update

--- add_notes.sh
+++ add_notes.sh
@@ -1,4 +1,4 @@
-python add_paper.py --tags "compress_SE" --notes "Learning both Weights and Connections for Efficient Neural Networks" --author "Song Han, Jeff Pool, William J. Dally" --pdf_address "https://arxiv.org/pdf/1506.02626.pdf" --years "2015"
+python add_paper.py --tags "ASR" --notes "A STREAMING ON-DEVICE END-TO-END MODEL SURPASSING SERVER-SIDE CONVENTIONAL MODEL QUALITY AND LATENCY" --author "Google" --pdf_address "https://arxiv.org/pdf/2003.12710.pdf" --years "2020.03"
 
 #python add_paper.py --tags "train_method" --notes "on_the_difficulty_of_training_rnn" --author "Razvan Pascanu, Tomas Mikolov, Yoshua Bengio" --pdf_address "https://arxiv.org/pdf/1211.5063.pdf" --years "2013"
 #python add_paper.py --tags "compress_SE" --notes "Increasing_Compactness_Of_Deep_Learning_Based_Speech_Enhancement_Models_With_Parameter_Pruning_And_Quantization_Techniques" --author "Jyun-Yi Wu, Cheng Yu, Szu-Wei Fu" --pdf_address "https://arxiv.org/pdf/1906.01078.pdf" --years "2019"
